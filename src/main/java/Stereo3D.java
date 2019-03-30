d467368950836a62cdf59861dac0abb00d946f19
CC <angelos.vanakaris@gmail.com>
2019-03-30 23:17:52 +0200

    Added Maven Dependencies.

--- src/main/java/Stereo3D.java
+++ src/main/java/Stereo3D.java
@@ -1,5 +1,12 @@
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 class Stereo3D {
 
@@ -31,6 +38,47 @@ class Stereo3D {
         System.out.println("3D stereo image created.");
 
         return stereo;
+
+    }
+
+    private static void convertMovietoJPG(String mp4Path, String imagePath, String imgType, int frameJump) throws Exception, IOException
+    {
+
+        Java2DFrameConverter converter = new Java2DFrameConverter();
+
+        FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(mp4Path);
+
+        frameGrabber.start();
+
+        Frame frame;
+
+        double frameRate=frameGrabber.getFrameRate();
+
+        int imgNum=0;
+
+        System.out.println("Video has "+frameGrabber.getLengthInFrames()+" frames and has frame rate of "+frameRate);
+
+        try {
+
+            for(int ii=1;ii<=frameGrabber.getLengthInFrames();ii++){
+
+                imgNum++;
+                frameGrabber.setFrameNumber(ii);
+                frame = frameGrabber.grab();
+                BufferedImage bi = converter.convert(frame);
+                String path = imagePath+File.separator+imgNum+".jpg";
+                ImageIO.write(bi,imgType, new File(path));
+                ii+=frameJump;
+
+            }
+
+            frameGrabber.stop();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
 
     }
 
