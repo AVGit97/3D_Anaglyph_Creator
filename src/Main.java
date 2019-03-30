1ac20abc9ce1914ed8d3703bda8838b4509df065
CC <angelos.vanakaris@gmail.com>
2019-03-30 22:04:42 +0200

    Organized code. Created method imageToStereo3D in Stereo3D class.

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,22 @@
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
 
-    private static final String LEFT_IMAGE = "left";
-    private static final String RIGHT_IMAGE = "right";
-    private static final String STEREO_IMAGE = "stereo";
-
     public static void main(String[] args) throws IOException {
 
-        File file = new File("images\\" + LEFT_IMAGE + ".jpg");
+        File file = new File("images\\left.jpg");
         BufferedImage left = ImageIO.read(file);
 
-        file = new File("images\\" + RIGHT_IMAGE + ".jpg");
+        file = new File("images\\right.jpg");
         BufferedImage right = ImageIO.read(file);
 
-        if (left.getWidth() != right.getWidth()) return;
-        if (left.getHeight() != right.getHeight()) return;
+        file = new File("images\\stereo.jpg");
+        BufferedImage stereo = Stereo3D.imageToStereo3D(left, right);
 
-        int width = left.getWidth(), height = left.getHeight();
-
-        BufferedImage stereo = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-        final Color red = new Color(255, 0, 0);
-        final Color cyan = new Color(0, 255, 255);
-
-        Color left_color, right_color;
-        for (int w = 0; w < width; w++) {
-            for (int h = 0; h < height; h++) {
-                left_color = new Color(subtract(new Color(left.getRGB(w, h)), cyan));
-
-                right_color = new Color(subtract(new Color(right.getRGB(w, h)), red));
-
-                stereo.setRGB(w, h, add(left_color, right_color));
-            }
-        }
-
-        file = new File("images\\" + STEREO_IMAGE + ".jpg");
-        ImageIO.write(stereo, "jpg", file);
-
-        System.out.println("3D stereo image created.");
-
-    }
-
-    private static int subtract(Color c1, Color c2) {
-
-        int r = c1.getRed() - c2.getRed() < 0 ? 0 : c1.getRed() - c2.getRed();
-        int g = c1.getGreen() - c2.getGreen() < 0 ? 0 : c1.getGreen() - c2.getGreen();
-        int b = c1.getBlue() - c2.getBlue() < 0 ? 0 : c1.getBlue() - c2.getBlue();
-
-        return new Color(r, g, b).getRGB();
-
-    }
-
-    private static int add(Color c1, Color c2) {
-
-        int r = c1.getRed() + c2.getRed() > 255 ? 255 : c1.getRed() + c2.getRed();
-        int g = c1.getGreen() + c2.getGreen() > 255 ? 255 : c1.getGreen() + c2.getGreen();
-        int b = c1.getBlue() + c2.getBlue() > 255 ? 255 : c1.getBlue() + c2.getBlue();
-
-        return new Color(r, g, b).getRGB();
+        if (stereo != null) ImageIO.write(stereo, "jpg", file);
 
     }
 
